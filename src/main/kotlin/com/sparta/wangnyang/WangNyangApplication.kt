9fa74625740e6f829a913fd47973d0c7d8eff5ad
zunbee <zunbeeok@gmail.com>
2024-01-08 14:35:07 +0900

    spring initial / 스프링 생성

---
+++ src/main/kotlin/com/sparta/wangnyang/WangNyangApplication.kt
@@ -0,0 +1,11 @@
+package com.sparta.wangnyang
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WangNyangApplication
+
+fun main(args: Array<String>) {
+    runApplication<WangNyangApplication>(*args)
+}
