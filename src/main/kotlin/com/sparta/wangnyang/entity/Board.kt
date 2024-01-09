bfb3ca4c581b3d05faf19c01180036951545b133
junesung <ranobell1026@naver.com>
2024-01-09 17:01:57 +0900

    엔티티ì - 보ëboard  ã수정

--- src/main/kotlin/com/sparta/wangnyang/entity/Board.kt
+++ src/main/kotlin/com/sparta/wangnyang/entity/Board.kt
@@ -1,18 +1,39 @@
 package com.sparta.wangnyang.entity
 
+import com.sparta.wangnyang.domain.board.dto.BoardResponse
 import com.sparta.wangnyang.common.BaseTimeEntity
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.Table
 
 
 @Entity
+@Table(name = "board")
 data class Board(
-        var title : String,
-        var mainText:String,
-        var userId:String,
+    @Column(name = "title")
+    var title : String,
+
+    @Column(name = "mainText")
+    var mainText:String,
+
+    @Column(name = "userId")
+    var userId:String,
+
+
 ):BaseTimeEntity(){
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? =null;
+}
+
+fun Board.toResponse(): BoardResponse {
+    return BoardResponse(
+        id = id!!,
+        title = title,
+        mainText = mainText,
+        writer = userId,
+        createdAt = createdAt.toString()
+    )
 }
